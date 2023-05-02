334b726c767104fb54556e440cefe789289ba031
George Giannakopoulos <ggianna@scify.org>
2023-05-02 20:34:51 +0300

    - Logger: added option to remove date.

--- structureDamagePrediction/utils.py
+++ structureDamagePrediction/utils.py
@@ -4,8 +4,11 @@ class StartEndLogger():
     def __init__(self) -> None:
         self.last_msg = ""
     
-    def log(self, msg):
-        print("%s: %s"%(datetime.now(), msg))
+    def log(self, msg, no_date = False):
+        if no_date:
+            print("%s"%(msg))
+        else:
+            print("%s: %s"%(datetime.now(), msg))
     
     def start(self, msg):
         self.last_msg = msg
